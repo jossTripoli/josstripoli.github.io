619410c5227f0d577256acc57904d0c43c0d8486
Joss <jct324@lehigh.edu>
2026-03-09 02:29:47 -0400

    mobile sticky fix

--- components/sticky-back-link.tsx
+++ components/sticky-back-link.tsx
@@ -41,7 +41,7 @@ export function StickyBackLink() {
   }, [triggerScrollY])
 
   return (
-    <div ref={stickyRef} className="container sticky top-0 z-40 mx-auto px-6 pt-5">
+    <div ref={stickyRef} className="container static xl:sticky top-0 z-40 mx-auto px-6 pt-5">
       <Link
         href="/#work"
         className={`inline-flex items-center gap-2 rounded-full px-4 py-2 text-sm font-medium text-foreground transition-all ${
